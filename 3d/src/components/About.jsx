a6614a91c4bc01b56623b1b6afb8b6617289dee7
Nicolas Pham <67529386+NicolasPham@users.noreply.github.com>
2023-05-11 19:09:53 +0000

    Update 4 files

--- 3d/src/components/About.jsx
+++ 3d/src/components/About.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import Tilt from "react-parallax-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview</h2>
+      </motion.div>
 
-export default About
+      <motion.p variants={fadeIn("", "", 0.1, 1)}>
+        I'm a skilled software developer with knowledge in frameworks such as
+        React, Nodejs. I'm a quick learner and will be able to collaborate
+        closely with clients and team members to create efficient, scalable, and
+        user-friendly solutions that solve real-world problem.
+      </motion.p>
+    </>
+  );
+};
+
+export default About;
--- 3d/src/components/canvas/Computers.jsx
+++ 3d/src/components/canvas/Computers.jsx
@@ -1,36 +1,64 @@
 import { Suspense, useState, useEffect } from "react";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import { OrbitControls, Preload, SpotLight, useGLTF } from "@react-three/drei";
 
-import CanvasLoader from "../Loader";
+import CanvasLoader from "../Loader"; //to show the percentage when image not loaded yet
 
-const Computers = () => {
+const Computers = ({ isMobile }) => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor="black" />
+      <hemisphereLight intensity={0.15} groundColor="black" /> 
       <pointLight intensity={1} />
-      <primitive object={computer.scene} />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <primitive
+        object={computer.scene}
+        scale={isMobile ? 0.7 : 0.75}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        rotation={[-0.01, 0.2, -0.1]}
+      />
     </mesh>
   );
 };
 
 const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    
+    //check if the window is on Mobile, then we can setIsMobile = true
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+    setIsMobile(mediaQuery.matches);
+  }, []);
+  console.log(isMobile);
+
   return (
     <Canvas
       frameloop="demand"
       shadows
       camera={{ position: [20, 3, 5], fov: 25 }}
-      gl={{ preserveDrawingBuffer: true }}>
-      <Suspense>
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense
+      // fallback={<CanvasLoader />}
+      >
         <OrbitControls
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers />
+        <Computers isMobile={isMobile} />
       </Suspense>
+
+      <Preload all />
     </Canvas>
   );
 };
