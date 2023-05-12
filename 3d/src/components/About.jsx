cf37fee39ecd3cf427c3f155b737405c58253440
Nicolas Pham <67529386+NicolasPham@users.noreply.github.com>
2023-05-12 02:16:44 +0000

    Update 5 files

--- 3d/src/components/About.jsx
+++ 3d/src/components/About.jsx
@@ -5,6 +5,33 @@ import { styles } from "../styles";
 import { services } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
 
+import { SectionWrapper } from "../hoc";
+// Service card ==============================
+const ServiceCard = ({ title, index, icon }) => {
+  return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        className="w-full p-[1px] rounded-[20px] shadow-card green-pink-gradient"
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+      >
+        <div
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex flex-col items-center justify-evenly"
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450,
+          }}
+        >
+          <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+          <h3 className="text-[20px] font-bold text-center">{title}</h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  );
+};
+
+// About Section =============================================
+
 const About = () => {
   return (
     <>
@@ -13,14 +40,23 @@ const About = () => {
         <h2 className={styles.sectionHeadText}>Overview</h2>
       </motion.div>
 
-      <motion.p variants={fadeIn("", "", 0.1, 1)}>
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary max-w-3xl leading-[30px] text-[17px]"
+      >
         I'm a skilled software developer with knowledge in frameworks such as
         React, Nodejs. I'm a quick learner and will be able to collaborate
         closely with clients and team members to create efficient, scalable, and
         user-friendly solutions that solve real-world problem.
       </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
     </>
   );
 };
 
-export default About;
+export default SectionWrapper(About, "about");
